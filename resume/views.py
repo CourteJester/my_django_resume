62df192c8a74a8515bfdcb886b458effda4753b2
Courte Jester <courtejester@gmail.com>
2021-05-03 07:57:17 -0700

    blanked-out About and Home1 pages (on views and urls)

--- resume/views.py
+++ resume/views.py
@@ -5,14 +5,14 @@ import datetime
 def home(request):
 	return render(request, 'home.html', {})
 	
-def home1(request):
-	from resume.name_file import names_function
-	names = ["Eric", "Stan", "Kyle", "Kenny"]
-	return render(request, 'home1.html', {'names': names, 'namer' : names_function})
+#def home1(request):
+#	from resume.name_file import names_function
+#	names = ["Eric", "Stan", "Kyle", "Kenny"]
+#	return render(request, 'home1.html', {'names': names, 'namer' : names_function})
 
-def about(request):
-	f_name = "Gargamel"
-	l_name = "Smith"
-	t_date = datetime.datetime.now()
-	return render(request, 'about-me.html', {'first_name': f_name, 'last_name': l_name, 'today_date': t_date })
+#def about(request):
+#	f_name = "Gargamel"
+#	l_name = "Smith"
+#	t_date = datetime.datetime.now()
+#	return render(request, 'about-me.html', {'first_name': f_name, 'last_name': l_name, 'today_date': t_date })
 
